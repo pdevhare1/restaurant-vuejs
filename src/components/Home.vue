b0ee4cf52a4d61e83f88cb08f271af608e2ae60d
Prathamesh Devhare <prathamesh.kali@gmail.com>
2023-07-08 18:27:25 +0530

    bug fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <NavBar />
-    <h1>Hey Welcome Home</h1>
+    <h1>Hey {{ name }} Welcome Home</h1>
   </div>
 </template>
 
@@ -9,11 +9,17 @@
 import NavBar from "./NavBar.vue";
 export default {
   name: "HomePage",
+  data() {
+    return {
+      name: "",
+    };
+  },
   components: {
     NavBar,
   },
   mounted() {
     const user = localStorage.getItem("user-info");
+    this.name = JSON.parse(user).name;
     if (!user) {
       this.$router.push({
         name: "SignUp",
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+// import { computed } from "vue";
 export default {
   name: "NavBar",
   methods: {
